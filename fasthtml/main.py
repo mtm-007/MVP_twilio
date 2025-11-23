0b66d3573b0bc86aee914b707f7a91a6435ac687
mtm_007 <45622655+mtm-007@users.noreply.github.com>
2025-11-23 09:02:12 +0000

    fix db insert title issue

--- fasthtml/main.py
+++ fasthtml/main.py
@@ -4,16 +4,15 @@ from fasthtml.common import *
 
 app, rt, todos, Todo = fast_app(
         'todos.db',live=True, 
-        id=int, title=str, done=bool, pk='id')
-        #tbls={"todos":dict(id=int, title=str, done=bool, pk='id')})
+        tbls={"todos":dict(id=int, title=str, done=bool, pk='id')})
 
 @rt('/')
 def get():
-    todos.insert(Todo(title="first todo", done=False))
-    items = todos()
-    #items = [Li(o) for o in todos()]
+    todos.insert(Todo(title="second todo", done=False))
+    #items = todos()
+    items = [Li(o) for o in todos()]
     return Titled('Todos List',
-                  Div(*items),
+                  Ul(*items),
                   )
 
 serve()
